771c49bb6c7ff5118c1b2b3318ea8790dd0901dd
nye <killernye@hotmail.com>
2021-03-20 20:52:52 +0800

    finish lab2

--- lab2/IntList/SquarePrimesTest.java
+++ lab2/IntList/SquarePrimesTest.java
@@ -17,4 +17,12 @@ public class SquarePrimesTest {
         assertEquals("14 -> 15 -> 16 -> 289 -> 18", lst.toString());
         assertTrue(changed);
     }
+
+    @Test
+    public void testSquarePrimeSimple2() {
+        IntList lst = IntList.of(-1, 0, 1, 2, 3, 5, 6, 10);
+        boolean changed = IntListExercises.squarePrimes(lst);
+        assertEquals("-1 -> 0 -> 1 -> 4 -> 9 -> 25 -> 6 -> 10", lst.toString());
+        assertTrue(changed);
+    }
 }
