c346d1dfdf2f541762e5c8ba6e707c3c0e1f0532
nye <killernye@hotmail.com>
2021-04-16 17:02:39 +0800

    finish lab6

--- lab6/capers/CapersRepository.java
+++ lab6/capers/CapersRepository.java
@@ -1,25 +1,27 @@
 package capers;
 
+//import edu.princeton.cs.algs4.ST;
+
 import java.io.File;
 import static capers.Utils.*;
 
 /** A repository for Capers 
- * @author TODO
+ * @author Nye
  * The structure of a Capers Repository is as follows:
  *
  * .capers/ -- top level folder for all persistent data in your lab12 folder
  *    - dogs/ -- folder containing all of the persistent data for dogs
  *    - story -- file containing the current story
- *
- * TODO: change the above structure if you do something different.
  */
 public class CapersRepository {
     /** Current Working Directory. */
     static final File CWD = new File(System.getProperty("user.dir"));
 
     /** Main metadata folder. */
-    static final File CAPERS_FOLDER = null; // TODO Hint: look at the `join`
-                                            //      function in Utils
+    static final File CAPERS_FOLDER = new File(".capers");
+
+    /** File where story writes. */
+    static final File STORY = new File(".capers/story.txt");
 
     /**
      * Does required filesystem operations to allow for persistence.
@@ -31,7 +33,14 @@ public class CapersRepository {
      *    - story -- file containing the current story
      */
     public static void setupPersistence() {
-        // TODO
+        if (!CWD.exists())
+            CWD.mkdir();
+        if (!CAPERS_FOLDER.exists())
+            CAPERS_FOLDER.mkdir();
+        if (!STORY.exists())
+            Utils.writeContents(STORY);
+        if (!Dog.DOG_FOLDER.exists())
+            Dog.DOG_FOLDER.mkdir();
     }
 
     /**
@@ -40,7 +49,11 @@ public class CapersRepository {
      * @param text String of the text to be appended to the story
      */
     public static void writeStory(String text) {
-        // TODO
+        String story = Utils.readContentsAsString(STORY);
+        StringBuffer sb = new StringBuffer(story);
+        sb.append(text + '\n');
+        Utils.writeContents(STORY, sb.toString());
+        System.out.println(sb.toString());
     }
 
     /**
@@ -49,7 +62,9 @@ public class CapersRepository {
      * Also prints out the dog's information using toString().
      */
     public static void makeDog(String name, String breed, int age) {
-        // TODO
+        Dog d = new Dog(name, breed, age);
+        d.saveDog();
+        System.out.println(d.toString());
     }
 
     /**
@@ -59,6 +74,8 @@ public class CapersRepository {
      * @param name String name of the Dog whose birthday we're celebrating.
      */
     public static void celebrateBirthday(String name) {
-        // TODO
+        Dog birthdayDog = Dog.fromFile(name);
+        birthdayDog.haveBirthday();
+        birthdayDog.saveDog();
     }
 }
--- lab6/capers/Dog.java
+++ lab6/capers/Dog.java
@@ -1,17 +1,18 @@
 package capers;
 
+
+
 import java.io.File;
 import java.io.Serializable;
 import static capers.Utils.*;
 
 /** Represents a dog that can be serialized.
- * @author TODO
+ * @author NYE
 */
-public class Dog { // TODO
+public class Dog implements java.io.Serializable {
 
     /** Folder that dogs live in. */
-    static final File DOG_FOLDER = null; // TODO (hint: look at the `join`
-                                         //      function in Utils)
+    static final File DOG_FOLDER = Utils.join(".capers", "dogs");
 
     /** Age of dog. */
     private int age;
@@ -39,8 +40,9 @@ public class Dog { // TODO
      * @return Dog read from file
      */
     public static Dog fromFile(String name) {
-        // TODO (hint: look at the Utils file)
-        return null;
+        File inFile = Utils.join(".capers", "dogs", name + ".txt");
+        Dog result = Utils.readObject(inFile, Dog.class);
+        return result;
     }
 
     /**
@@ -56,7 +58,8 @@ public class Dog { // TODO
      * Saves a dog to a file for future use.
      */
     public void saveDog() {
-        // TODO (hint: don't forget dog names are unique)
+        File outfile = Utils.join(".capers", "dogs", this.name + ".txt");
+        Utils.writeObject(outfile, this);
     }
 
     @Override
